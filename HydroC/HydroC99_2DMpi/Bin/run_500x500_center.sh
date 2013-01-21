3c9359a41f84f518b8ae06c79dfb8b7f1f10844a
Guillaume Colin de Verdiere <guillaume.colin-de-verdiere@cea.fr>
2013-01-21 15:17:38 +0100

    threads localized on the proper socket

--- HydroC/HydroC99_2DMpi/Bin/run_500x500_center.sh
+++ HydroC/HydroC99_2DMpi/Bin/run_500x500_center.sh
@@ -3,11 +3,12 @@
 RUNDIR=${HOME}/ptmp/Github/HydroC/HydroC99_2DMpi
 EXEDIR=${PWD}/../Src/
 INPDIR=${PWD}/../../../Input
-# RUNCMD="ccc_mprun -p standard -n 8 -N 4 -x "
+RUNCMD="env OMP_NUM_THREADS=4 KMP_AFFINITY=compact ccc_mprun -p standard -n 8 -N 4 -x -c 4 "
 
 
 mkdir -p ${RUNDIR}
 cd ${RUNDIR}
+set -x
 ${RUNCMD} ${EXEDIR}/hydro -i ${INPDIR}/input_500x500_center.nml 
 
 #EOF
