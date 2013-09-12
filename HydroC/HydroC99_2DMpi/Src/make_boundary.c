a35d0ba39a1a7c1f919b89a5721f17dd4d421240
Myname <me@example.com>
2013-09-12 16:54:13 +0200

    FTI integration

--- HydroC/HydroC99_2DMpi/Src/make_boundary.c
+++ HydroC/HydroC99_2DMpi/Src/make_boundary.c
@@ -3,6 +3,7 @@
   (C) Romain Teyssier : CEA/IRFU           -- original F90 code
   (C) Pierre-Francois Lavallee : IDRIS      -- original F90 code
   (C) Guillaume Colin de Verdiere : CEA/DAM -- for the C version
+  (C) Adèle Villiermet : CINES            -- for FTI integration
 */
 /*
 
