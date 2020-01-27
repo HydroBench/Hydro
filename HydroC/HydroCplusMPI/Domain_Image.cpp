ba6bfee21cfeb545ed862fb56096be6f4f7bd76c
Guillaume Colin de Verdiere <guillaume.colin-de-verdiere@cea.fr>
2020-01-27 13:02:56 +0100

    use env var HYDROC_IMG_PREFIX to add a prefix to the PNG|PPM images

--- HydroC/HydroCplusMPI/Domain_Image.cpp
+++ HydroC/HydroCplusMPI/Domain_Image.cpp
@@ -467,10 +467,27 @@ void Domain::pngWriteFile(char *name)
 	imgSizeX = m_globNx / m_shrink;
 	imgSizeY = m_globNy / m_shrink;
 
-#if WITHPNG == 0
+	// Open image file
 	if (m_myPe == 0) {
-		// cerr << "shrink factor " << m_shrink << " " << name << endl;
-		m_fp = fopen(name, "w");
+		char *imPrefix = getenv("HYDROC_IMG_PREFIX");
+		char imgname[1024];
+		strcpy(imgname, name);
+		if (imPrefix == NULL) {
+			m_fp = fopen(name, "w");
+		} else {
+			sprintf(imgname, "%s%s", imPrefix, name);
+			m_fp = fopen(imgname, "w");
+		}
+		if (!m_fp) {
+#ifdef MPI_ON
+			fprintf(stderr, "File %s could not be opened for writing\n", imgname);
+			MPI_Abort(MPI_COMM_WORLD, 1);
+#else
+			abort_("File %s could not be opened for writing", imgname);
+#endif
+		}
+		
+#if WITHPNG == 0
 #define BINARY 1
 #if BINARY == 1
 		fprintf(m_fp, "P6\n");
@@ -484,7 +501,7 @@ void Domain::pngWriteFile(char *name)
 			}
 		}
 		fprintf(m_fp, "#EOF\n");
-#else
+#else // BINARY != 1
 		fprintf(m_fp, "P3\n");
 		fprintf(m_fp, "%d %d\n", imgSizeX, imgSizeY);
 		fprintf(m_fp, "255\n");
@@ -497,45 +514,40 @@ void Domain::pngWriteFile(char *name)
 			fprintf(m_fp, "\n");
 		}
 		fprintf(m_fp, "#EOF\n");
-#endif
-	}
-#else
-	png_byte color_type = PNG_COLOR_TYPE_RGBA;
-	png_byte bit_depth = 8;
-
-	/* create file */
-	if (m_myPe == 0) {
+#endif // BINARY != 1
+#else // WITHPNG != 0
+		png_byte color_type = PNG_COLOR_TYPE_RGBA;
+		png_byte bit_depth = 8;
+		
+		/* create file */
 		// cerr << "shrink factor " << m_shrink << " " << name << endl;
-		m_fp = fopen(name, "w");
-		if (!m_fp)
-			abort_("[write_png_file] File %s could not be opened for writing", name);
-
+		
 		/* initialize stuff */
 		m_png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-
+		
 		if (!m_png_ptr)
 			abort_("[write_png_file] png_create_write_struct failed");
-
+		
 		m_info_ptr = png_create_info_struct(m_png_ptr);
 		if (!m_info_ptr)
 			abort_("[write_png_file] png_create_info_struct failed");
-
+		
 		// if (setjmp(png_jmpbuf(m_png_ptr)))
 		//      abort_("[write_png_file] Error during init_io");
-
+		
 		png_init_io(m_png_ptr, m_fp);
 		/* write header */
 		// if (setjmp(png_jmpbuf(m_png_ptr)))
 		//      abort_("[write_png_file] Error during writing header");
-
+		
 		png_set_IHDR(m_png_ptr, m_info_ptr, imgSizeX, imgSizeY, bit_depth, color_type, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-
+		
 		png_write_info(m_png_ptr, m_info_ptr);
 		// if (m_myPe == 0) cerr << "Header of final image written " << m_globNx << " " << m_globNy << endl;
+#endif // WITHPNG != 0
 	}
 #ifdef MPI_ON
 	MPI_Barrier(MPI_COMM_WORLD);
-#endif
 #endif
 }
 
