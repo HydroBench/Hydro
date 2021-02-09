369b23af18c3a0ec345099ce0363fc5a07237d98
Guillaume Colin de Verdiere <Guillaume.Colin-de-Verdiere@cea.fr>
2021-02-09 10:39:07 +0100

    deactivate source formatting thru .pre-commit.sh using indent to favor clang-format which is more modern C++ compliant

--- .pre-commit.sh
+++ .pre-commit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git diff --name-only HEAD | egrep '.*\.C|.*\.c|.*\.h|.*\.*pp'
-git diff --name-only HEAD | egrep '.*\.C|.*\.c|.*\.h|.*\.*pp'| xargs indent
+# try to move to clang-format instead of old indent
+# git diff --name-only HEAD | egrep '.*\.C|.*\.c|.*\.h|.*\.*pp'| xargs indent
 #EOF
